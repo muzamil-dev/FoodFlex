64b2063f5332c5cb8e1b5ff177e5bcf9ac461360
AishaFathalla <156489851+AishaFathalla@users.noreply.github.com>
2024-10-05 01:22:32 -0400

    project setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import RecipeList from './components/RecipeList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/RecipesList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const RecipeList = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/recipes/')
+      .then(response => {
+        setRecipes(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>{recipe.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipeList;
